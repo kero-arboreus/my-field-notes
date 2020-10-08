b36d8643bd51f05705c531c808a02a22c7d370da
kero-arboreus <0yuka.odachi0@gmail.com>
2020-10-08 12:47:33 +0900

    ストロングパラメーター〜バリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'include both letters and numbers' }
+
+  with_options presence: true do
+    validates :nickname
+    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'full-width characters' }
+    validates :name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'full-width katakana characters' }
+    validates :user_address, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'full-width characters' }
+    validates :phone,numericality: { only_integer: true }, length: { maximum: 11 }
+  end
 end
