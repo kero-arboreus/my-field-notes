6181fbddeb5c6aeba53d0c087fae794e3ff547de
kero-arboreus <0yuka.odachi0@gmail.com>
2020-10-11 20:12:43 +0900

    記事投稿機能を実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,26 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.includes(:user).order("created_at DESC")
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:image, :title, :date, :time_zone_id, :prefecture_id, :category_id, :text, :address, :latitude, :longitude, :parking, :crowd, :toilet).merge(user_id: current_user.id)
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,8 +3,18 @@ class Article < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :time_zone_id
-  belongs_to_active_hash :prefecture_id
-  belongs_to_active_hash :category_id
+  belongs_to_active_hash :time_zone
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :category
+
+  with_options presence: true do
+    validates :title, length: { maximum: 30 }
+    validates :date
+    validates :time_zone_id, numericality: { other_than: 1, message: 'select' }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'select' }
+    validates :category_id, numericality: { other_than: 1, message: 'select' }
+    validates :text
+    validates :address
+  end
 
 end
