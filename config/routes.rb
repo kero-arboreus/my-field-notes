6c7326bb7bd05bf9b2ec604e7ea5a98c8d99df5d
kero-arboreus <0yuka.odachi0@gmail.com>
2020-10-15 15:34:51 +0900

    ルーティング、コントローラーの記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'comments/new'
   devise_for :users
   root to: 'articles#index'
+  resources :users, only: [:index, :show, :edit, :update]
   resources :articles do
     resources :comments,only: :create
   end
