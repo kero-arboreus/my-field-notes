381aa7272dc8f4219c8245fa7f9a504459b9c5b3
kero-arboreus <70256474+kero-arboreus@users.noreply.github.com>
2020-10-17 15:56:26 +0900

    Revert "Revert "Open weather apiの導入""

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :prefecture_id])
   end
 
   def search_article
